02db03e85c1d2107ae34f6d418563efdb767b10a
Bluub <louis.auzuret@gmail.com>
2021-12-20 14:27:29 +0100

    rofi binaries in PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,8 @@ set -gx VISUAL "nvim"
 set -gx EDITOR "nvim"
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
-set PATH $PATH "/home/bluub/my_scripts"
+set PATH $PATH "$HOME/my_scripts"
+set PATH $PATH "$HOME/.config/rofi/bin"
 set MANPATH $NPM_PACKAGES/share/man $MANPATH
 
 alias ionised 'fish -c "nmcli device wifi connect D8:54:A2:01:05:24 &> /dev/null &"'
