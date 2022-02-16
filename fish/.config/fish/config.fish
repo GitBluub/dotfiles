7794a403cb1de58102b6d0641720d1946b853452
Bluub <louis.auzuret@gmail.com>
2022-02-16 18:43:04 +0100

    new rofi, +fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,7 @@ set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 set PATH $PATH "$HOME/my_scripts"
 set PATH $PATH "$HOME/.config/rofi/bin"
+set PATH $PATH "$HOME/.ghcup/bin"
 
 alias ionised 'fish -c "nmcli device wifi connect D8:54:A2:01:05:24 &> /dev/null &"'
 alias icat "kitty +kitten icat"
