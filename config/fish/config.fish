afcf5f8a9a3021106ae0315c273dc87be5fdadc5
GitBluub <louis.auzuret@gmail.com>
2022-09-28 13:30:24 +0900

    fix: posix source

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,12 @@
 # ~/.config/fish/config.fish
 
+function posix-source
+	for i in (cat $argv)
+		set arr (echo $i |tr = \n)
+  		set -gx $arr[1] $arr[2]
+	end
+end
+
 set -gx VISUAL "nvim"
 set -gx EDITOR "nvim"
 set NPM_PACKAGES "$HOME/.npm-global"
@@ -8,6 +15,12 @@ set PATH $PATH "$HOME/bin"
 set PATH $PATH "$HOME/.config/rofi/bin"
 set PATH $PATH "$HOME/.ghcup/bin"
 set PATH $PATH "$HOME/.local/bin"
+
+#  IBUS  (input  processor  (for  Japanese))
+set  GTK_IM_MODULE "fcitx"
+set  QT_IM_MODULE "fcitx"
+set  XMODIFIERS "@im=fcitx"
+set  GLFW_IM_MODULE "ibus"
 
 alias k "kubectl"
 alias g "git"
