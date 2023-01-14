d701ff03768005c46a9759fd236affde56396c01
GitBluub <louis.auzuret@gmail.com>
2023-01-15 02:45:28 +0900

    Switch to zsh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ stow config -t $HOME/.config
 stow tmux -t $HOME
 stow bin -t $HOME/bin
 stow wallpapers -t $HOME/wallpapers
+ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 
 # fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
