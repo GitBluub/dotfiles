d5d995d82201b59d0bc830de8573f4bf2489a033
GitBluub <louis.auzuret@gmail.com>
2023-01-15 01:44:48 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ stow config -t $HOME/.config
 stow tmux -t $HOME
 stow bin -t $HOME/bin
 stow wallpapers -t $HOME/wallpapers
+
+# fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
