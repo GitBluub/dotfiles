73732ea2d980e33006ee88db64b5be485d984b48
GitBluub <louis.auzuret@gmail.com>
2023-01-15 16:03:24 +0900

    gitconfig symlink

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ stow tmux -t $HOME
 stow bin -t $HOME/bin
 stow wallpapers -t $HOME/wallpapers
 ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 
 # fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
